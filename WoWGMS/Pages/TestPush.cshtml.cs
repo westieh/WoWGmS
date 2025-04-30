3bbe1e2ab9db395582f1b30f89b79b39b69ea83d
westieh <theodor.kaalund@hotmail.com>
2025-04-30 10:08:14 +0200

    TestPush

---
+++ WoWGMS/Pages/TestPush.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WoWGMS.Pages
+{
+    public class TestPushModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
