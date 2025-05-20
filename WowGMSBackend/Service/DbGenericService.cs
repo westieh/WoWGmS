85e6b454a379e2fbf7a918edbecf53c8485776dd
Tjobse <TobiBertelsen@yahoo.dk>
2025-05-20 11:40:54 +0200

    CRUD-methods DBService

---
+++ WowGMSBackend/Service/DbGenericService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WowGMSBackend.DBContext;
+
+namespace WowGMSBackend.Service
+{
+    public class DbGenericService<T> : IDBService<T> where T : class
+    {
+        public async Task AddObjectAsync(T obj)
+        {
+            using (var context = new WowDbContext())
+            {
+                context.Set<T>().Add(obj);
+                await context.SaveChangesAsync();
+            }
+        }
+        public async Task DeleteObjectAsync(T obj)
+        {
+            using (var context = new WowDbContext())
+            {
+                context.Set<T>().Remove(obj);
+                await context.SaveChangesAsync();
+            }
+        }
+        public async Task UpdateObjectAsync(T obj)
+        {
+            using (var context = new WowDbContext())
+            {
+                context.Set<T>().Update(obj);
+                await context.SaveChangesAsync();
+            }
+        }
+        public async Task<IEnumerable<T>> GetAllObjectsAsync()
+        {
+            using (var context = new WowDbContext())
+            {
+                return await context.Set<T>().AsNoTracking().ToListAsync();
+            }
+        }
+        public async Task<T> GetObjectByIdAsync(params object[] keyValues)
+        {
+            using (var context = new WowDbContext())
+            {
+                return await context.Set<T>().FindAsync(keyValues);
+            }
+        }
+        public async Task SaveObjects(List<T> objects)
+        {
+            using (var context = new WowDbContext())
+            {
+                foreach (T obj in objects)
+                {
+                    context.Set<T>().Add(obj);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ WowGMSBackend/Service/IDBService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WowGMSBackend.Service
+{
+    public interface IDBService<T> where T : class
+    {
+        Task AddObjectAsync(T obj);
+        Task DeleteObjectAsync(T obj);
+        Task UpdateObjectAsync(T obj);
+        Task<IEnumerable<T>> GetAllObjectsAsync();
+        Task<T> GetObjectByIdAsync(params object[] keyValues);
+    }
+}
