0a8f0117dfebf36700aff6835d243c18f545bd6f
Simon Jensen <Simon.jensen.b@gmail.com>
2025-05-21 11:28:24 +0200

    ViewApplications(submitted applications) gjort admin only

--- WoWGMS/Pages/ViewApplications.cshtml.cs
+++ WoWGMS/Pages/ViewApplications.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,6 +8,7 @@ using WoWGMS.Repository;
 using WowGMSBackend.MockData;
 using WowGMSBackend.Service;
 
+[Authorize(Roles = "Admin")]
 public class ViewApplicationsModel : PageModel
 {
     private readonly IApplicationService _applicationService;
