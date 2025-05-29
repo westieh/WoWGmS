be6c110126a5f18c5f5040c12d48514a0727e830
westieh <theodor.kaalund@hotmail.com>
2025-05-29 02:37:30 +0200

    Fixed manage characters

--- WoWGMS/Pages/Admin/ManageCharacters.cshtml.cs
+++ WoWGMS/Pages/Admin/ManageCharacters.cshtml.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using WowGMSBackend.Interfaces;
 using WowGMSBackend.Model;
+using static WoWGMS.Pages.MemberPanelModel;
+using WowGMSBackend.Registry;
 
 namespace WoWGMS.Pages.Admin
 {
@@ -22,22 +24,75 @@ namespace WoWGMS.Pages.Admin
         public List<Character> Characters { get; set; } = new();
         [BindProperty]
         public int CharacterId { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SelectedBossSlug { get; set; }
+        [BindProperty]
+        public Dictionary<int, int> KillUpdates { get; set; } = new();
 
+        public Dictionary<int, List<CharacterWithKill>> GroupedCharacters { get; set; } = new();
+        public List<Boss> AllBosses { get; set; } = new();
         [BindProperty]
         public List<BossKill> BossKills { get; set; } = new();
         public void OnGet()
         {
-            Characters = _characterService.GetAllCharactersWithMemberAndBossKills();
+            AllBosses = RaidRegistry.Raids
+                .SelectMany(r => r.Bosses)
+                .Where(b => b != null && b.Slug != null && b.DisplayName != null)
+                .ToList();
+
+            if (string.IsNullOrEmpty(SelectedBossSlug))
+                SelectedBossSlug = "vexie-and-the-geargrinders";
+
+            var allChars = _characterService.GetAllCharactersWithMemberAndBossKills();
+
+            var withKills = allChars.Select(c =>
+            {
+                var kills = c.BossKills.Where(k => k.BossSlug == SelectedBossSlug).ToList();
+                return new CharacterWithKill
+                {
+                    Character = c,
+
+                    KillCount = kills.Sum(k => k.KillCount)
+                };
+            });
+
+            GroupedCharacters = withKills
+                .GroupBy(c => c.Character.MemberId)
+                .ToDictionary(g => g.Key, g => g.ToList());
         }
-        public IActionResult OnPostUpdateBossKills()
+
+        public IActionResult OnPostDeleteCharacter(int id)
         {
-            if (BossKills == null || CharacterId == 0)
-                return BadRequest();
+            _characterService.DeleteCharacter(id);
+            return RedirectToPage(new { SelectedBossSlug });
+        }
 
-            _bossKillService.SetBossKillsForCharacter(CharacterId, BossKills);
+        public class CharacterWithKill
+        {
+            public Character Character { get; set; }
+            public int KillCount { get; set; }
+        }
+        public IActionResult OnPostUpdateKills(string selectedBossSlug)
+        {
+            if (string.IsNullOrEmpty(selectedBossSlug))
+                selectedBossSlug = "vexie-and-the-geargrinders";
 
-            TempData["SuccessMessage"] = "Boss kills updated.";
-            return RedirectToPage();
+            foreach (var entry in KillUpdates)
+            {
+                int characterId = entry.Key;
+                int newCount = entry.Value;
+
+                _bossKillService.SetBossKillsForCharacter(characterId, new List<BossKill>
+        {
+            new BossKill
+            {
+                BossSlug = selectedBossSlug,
+                KillCount = newCount
+            }
+        });
+            }
+
+            return RedirectToPage(new { SelectedBossSlug = selectedBossSlug });
         }
     }
 }
